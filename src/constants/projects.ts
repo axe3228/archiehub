a040e7c5287b127c4dd0314a867bb8d682de94cc
Archie Habaradas <archiehabaradas3228@gmail.com>
2025-01-09 19:14:09 +0800

    fix: removed some projects on the list

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -37,45 +37,5 @@ export const Projects = [
         desc: `I was assigned to create and maintain multiple web pages as part of the project, 
         collaborating closely with the team to ensure cohesive design and performance.`,
         haveLink: true
-    },
-    {
-        id: 5,
-        image: "/images/projects/cogitate.png",
-        title: "Cogitate",
-        url: "https://www.cogitate.io/",
-        company: "@ Cogitate (2023)",
-        desc: `I handled the maintenance and updating of the web project, 
-        ensuring it remained aligned with company needs and industry standards.`,
-        haveLink: true
-    },
-    {
-        id: 6,
-        image: "/images/projects/negum-tempo-jp.png",
-        title: "Negum Tempo",
-        url: "https://negum-tenpo.com/",
-        company: "@ Grow Forward JP Inc. (2022)",
-        desc: `I was assigned to maintain and update the web project for our sister company, 
-        ensuring it met their evolving needs and standards.`,
-        haveLink: true
-    },
-    {
-        id: 7,
-        image: "/images/projects/growforwardjp.png",
-        title: "Grow Forward JP Inc.",
-        url: "https://growforwardjp.com/",
-        company: "@ Grow Forward JP Inc. (2021)",
-        desc: `I was tasked with maintaining and updating our company's website, 
-        focusing on improving functionality and user engagement.`,
-        haveLink: true
-    },
-    {
-        id: 8,
-        image: "/images/projects/negumjp.png",
-        title: "Negum",
-        url: "https://negum-jp.com/",
-        company: "@ Grow Forward JP Inc. (2021)",
-        desc: `In my role, I worked on the web project for our sister company, 
-        managing ongoing maintenance and implementing necessary updates.`,
-        haveLink: true
-    },
+    }
 ]
